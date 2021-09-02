8660d976af3c1890cb7fb75632b7812c0767ee14
pratham82 <mali.prathamesh82@gmail.com>
2021-09-02 23:49:27 +0530

    Added questions and answers data

---
+++ data.js
@@ -0,0 +1,58 @@
+const quizData = [
+  {
+    question:
+      '1) What happened to Anakin Skywalker during the battle with Count Dooku?',
+    answers:
+      '\n1. He lost his left leg\n2. He lost his right arm\n3. He lost his right leg\n4. He lost\n',
+  },
+  {
+    question: '2) Who played the part of Commander Cody?',
+    answers:
+      '\n1. Jay Laga’aia\n2. Temuera Morrison\n3. Ahmed Best\n4. Joel Edgerton\n',
+  },
+  {
+    question: '3) What did Luke Skywalker lose in his fight with Darth Vader?',
+    answers:
+      '\n1. His left hand\n2. His left foot\n3. His right hand\n4. His left leg\n',
+  },
+
+  {
+    question:
+      '4) According to the Emperor, what was Luke Skywalker’s weakness?',
+    answers:
+      '\n1. His faith in the Light Side of the Force\n2. His faith in his friends\n3. His lack of vision\n4. His resistance to the Dark Side of the Force\n',
+  },
+  {
+    question: '5) Where did the Clone Wars begin?',
+    answers: '\n1. Tatooine\n2. Geonosis\n3. Naboo\n4. Coruscant\n',
+  },
+  {
+    question:
+      '6) Which Star Wars film has this quote: “I’ve been in this fight since I was six years old!”',
+    answers:
+      '\n1. Star Wars: A New Hope\n2. Star Wars: The Rise of Skywalker\n3. Rogue One: A Star Wars Story\n4. Solo: A Star Wars Story\n',
+  },
+  {
+    question:
+      '7) What did Jar Jar Binks end up owing Qui-Gon Jinn after being rescued by the same during the invasion of Naboo?',
+    answers:
+      '\n1. A trip to Otoh Gunga\n2. A Bongo\n3. An honor debt\n4. 9,000 credits\n',
+  },
+  {
+    question: '8) What did Owen Lars tell Luke Skywalker about his father?',
+    answers:
+      '\n1. He had been a Jedi Knight\n2. He had been a Sith Lord\n3. He was a navigator on a spice freighter\n4. He was a fighter pilot\n',
+  },
+  {
+    question: '9) Who said this quote: “I choose to live for my people.”',
+    answers:
+      '\n1. Padmé Amidala\n2. Riyo Chuchi\n3. Queen Jamillia\n4. Hera Syndulla\n',
+  },
+  {
+    question: '10) What is Chewbacca’s weapon of choice?',
+    answers: '\n1. Blaster rifle\n2. Lightsaber\n3. Metal club\n4. Bowcaster\n',
+  },
+]
+const answers = ['2', '2', '3', '2', '2', '3', '3', '3', '2', '4']
+
+module.exports = { quizData, answers }
